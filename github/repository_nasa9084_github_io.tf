dac1c534083af2e25395811113ab33b502b500a9
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2024-01-30 22:48:47 +0900

    (squash)

--- github/repository_nasa9084_github_io.tf
+++ github/repository_nasa9084_github_io.tf
@@ -4,5 +4,20 @@ import {
 }
 
 resource "github_repository" "nasa9084_github_io" {
-  name = "nasa9084.github.io"
+  name        = "nasa9084.github.io"
+  description = "resume"
+
+  has_issues    = true
+
+  homepage_url = "https://nasa9084.github.io"
+
+  pages {
+    build_type = "legacy"
+    cname      = "resume.web-apps.tech"
+
+    source {
+      branch = "master"
+      path   = "/"
+    }
+  }
 }
