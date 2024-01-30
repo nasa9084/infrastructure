32a1e3647c464b067c0114024520eade9a5f26e5
nasa9084 <nasa9084@users.noreply.github.com>
2024-01-30 22:51:48 +0900

    Import nasa9084.github.io (#15)
    * Import nasa9084.github.io
    
    * (squash)
    
    * (squash) make fmt

---
+++ github/repository_nasa9084_github_io.tf
@@ -0,0 +1,23 @@
+import {
+  to = github_repository.nasa9084_github_io
+  id = "nasa9084.github.io"
+}
+
+resource "github_repository" "nasa9084_github_io" {
+  name        = "nasa9084.github.io"
+  description = "resume"
+
+  has_issues = true
+
+  homepage_url = "https://nasa9084.github.io"
+
+  pages {
+    build_type = "legacy"
+    cname      = "resume.web-apps.tech"
+
+    source {
+      branch = "master"
+      path   = "/"
+    }
+  }
+}
