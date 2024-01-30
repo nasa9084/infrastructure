25d8a5b557cd166ceb296921363dbeb32ee32aa3
nasa9084 <nasa9084@users.noreply.github.com>
2024-01-30 21:54:37 +0900

    Enable issues for this repository (#11)

--- github/repository_blog_web_apps_tech.tf
+++ github/repository_blog_web_apps_tech.tf
@@ -1,14 +1,14 @@
 resource "github_repository" "blog_web_apps_tech" {
-  name                                    = "blog.web-apps.tech"
+  name = "blog.web-apps.tech"
 
   # only allow squash merge
-  allow_merge_commit                      = false
-  allow_rebase_merge                      = false
+  allow_merge_commit = false
+  allow_rebase_merge = false
 
-  archive_on_destroy                      = true
-  delete_branch_on_merge                  = true
+  archive_on_destroy     = true
+  delete_branch_on_merge = true
 
-  has_issues                              = true
+  has_issues = true
 
   pages {
     build_type = "workflow"
