e0790e55d32769b56594a044e28c8cd0d15b5fe7
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2024-02-03 00:15:25 +0900

    (squash) fix

--- github/repository_go_switchbot.tf
+++ github/repository_go_switchbot.tf
@@ -8,6 +8,7 @@ resource "github_repository" "go_switchbot" {
   description = "SwitchBot API Client for Go"
   topics      = ["switchbot", "switchbot-api"]
 
+  has_issues = true
 
   delete_branch_on_merge = true
   allow_merge_commit     = false
