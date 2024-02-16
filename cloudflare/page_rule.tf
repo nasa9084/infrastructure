001154235feba2646fe2d36d3d552235ebf90700
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2024-02-17 01:06:19 +0900

    Remove import blocks

--- cloudflare/page_rule.tf
+++ cloudflare/page_rule.tf
@@ -1,13 +1,3 @@
-import {
-  to = cloudflare_page_rule.root
-  id = "08db5577586403ed45dd2981644cb873/4313b516b3533bffe627036897de17be"
-}
-
-import {
-  to = cloudflare_page_rule.www
-  id = "08db5577586403ed45dd2981644cb873/6e69c393c72e8dd0e16a99f5c4c4d751"
-}
-
 resource "cloudflare_page_rule" "root" {
   zone_id  = data.cloudflare_zone.web_apps_tech.id
   priority = 1
