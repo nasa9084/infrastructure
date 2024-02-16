0d34ceba540d91e66fa35a6c78c851baf482d5fc
nasa9084 <nasa9084@users.noreply.github.com>
2024-02-17 01:03:25 +0900

    Manage resources on cloudflare.com (#20)
    * add cloudflare provider
    
    * fix indent
    
    * add resources
    
    * make format

---
+++ cloudflare/page_rule.tf
@@ -0,0 +1,35 @@
+import {
+  to = cloudflare_page_rule.root
+  id = "08db5577586403ed45dd2981644cb873/4313b516b3533bffe627036897de17be"
+}
+
+import {
+  to = cloudflare_page_rule.www
+  id = "08db5577586403ed45dd2981644cb873/6e69c393c72e8dd0e16a99f5c4c4d751"
+}
+
+resource "cloudflare_page_rule" "root" {
+  zone_id  = data.cloudflare_zone.web_apps_tech.id
+  priority = 1
+
+  target = "web-apps.tech/*"
+  actions {
+    forwarding_url {
+      status_code = 301
+      url         = "https://blog.web-apps.tech/$1"
+    }
+  }
+}
+
+resource "cloudflare_page_rule" "www" {
+  zone_id  = data.cloudflare_zone.web_apps_tech.id
+  priority = 2
+
+  target = "www.web-apps.tech/*"
+  actions {
+    forwarding_url {
+      status_code = 301
+      url         = "https://blog.web-apps.tech/$1"
+    }
+  }
+}
---
+++ cloudflare/terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "~> 1.7.1"
+
+  cloud {
+    organization = "nasa9084"
+
+    workspaces {
+      name = "cloudflare"
+    }
+  }
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "cloudflare" {}
