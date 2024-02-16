8af763640c477f7970fbf14906b17e44ea3323a7
nasa9084 <nasa9084@users.noreply.github.com>
2024-02-17 01:19:58 +0900

    Use Terraform 1.7.3 (#21)
    * Use Terraform 1.7.3
    
    * (squash) fix

--- github/repository_mastering_go_std_packages.tf
+++ github/repository_mastering_go_std_packages.tf
@@ -1,9 +1,21 @@
 resource "github_repository" "mastering_go_std_packages" {
   name       = "mastering-go-std-packages"
   visibility = "private"
+
+  template {
+    include_all_branches = false
+    owner                = "TechBooster"
+    repository           = "ReVIEW-Template"
+  }
 }
 
 resource "github_repository" "mastering_go_std_packages_2" {
   name       = "mastering-go-std-packages-2"
   visibility = "private"
+
+  template {
+    include_all_branches = false
+    owner                = "TechBooster"
+    repository           = "ReVIEW-Template"
+  }
 }
