28a5cceea59803cec91a53e3848073ed075423ea
nasa9084 <nasa9084@users.noreply.github.com>
2025-02-17 23:36:17 +0900

    Update value->content (#59)
    * Update value->content
    
    * fmt

--- cloudflare/dns_records.tf
+++ cloudflare/dns_records.tf
@@ -25,9 +25,9 @@ resource "cloudflare_zone_dnssec" "web_apps_tech" {
 resource "cloudflare_record" "a_web_apps_tech" {
   zone_id = data.cloudflare_zone.web_apps_tech.id
 
-  name  = "web-apps.tech"
-  type  = "A"
-  value = "192.0.2.1"
+  name    = "web-apps.tech"
+  type    = "A"
+  content = "192.0.2.1"
 
   comment = "page rule"
 
@@ -37,9 +37,9 @@ resource "cloudflare_record" "a_web_apps_tech" {
 resource "cloudflare_record" "a_www" {
   zone_id = data.cloudflare_zone.web_apps_tech.id
 
-  name  = "www"
-  type  = "A"
-  value = "192.0.2.1"
+  name    = "www"
+  type    = "A"
+  content = "192.0.2.1"
 
   comment = "page rule"
 
@@ -53,19 +53,19 @@ resource "cloudflare_record" "a_www" {
 resource "cloudflare_record" "cname_blog" {
   zone_id = data.cloudflare_zone.web_apps_tech.id
 
-  name  = "blog"
-  type  = "CNAME"
-  value = "nasa9084.github.io"
-  ttl   = 60
+  name    = "blog"
+  type    = "CNAME"
+  content = "nasa9084.github.io"
+  ttl     = 60
 }
 
 resource "cloudflare_record" "cname_resume" {
   zone_id = data.cloudflare_zone.web_apps_tech.id
 
-  name  = "resume"
-  type  = "CNAME"
-  value = "nasa9084.github.io"
-  ttl   = 120
+  name    = "resume"
+  type    = "CNAME"
+  content = "nasa9084.github.io"
+  ttl     = 120
 }
 
 // //////////////////////////////
@@ -77,7 +77,7 @@ resource "cloudflare_record" "mx_web_apps_tech_1" {
 
   name     = "web-apps.tech"
   type     = "MX"
-  value    = "route1.mx.cloudflare.net"
+  content  = "route1.mx.cloudflare.net"
   ttl      = 1
   priority = 90
 }
@@ -88,7 +88,7 @@ resource "cloudflare_record" "mx_web_apps_tech_2" {
 
   name     = "web-apps.tech"
   type     = "MX"
-  value    = "route2.mx.cloudflare.net"
+  content  = "route2.mx.cloudflare.net"
   ttl      = 1
   priority = 50
 }
@@ -99,7 +99,7 @@ resource "cloudflare_record" "mx_web_apps_tech_3" {
 
   name     = "web-apps.tech"
   type     = "MX"
-  value    = "route3.mx.cloudflare.net"
+  content  = "route3.mx.cloudflare.net"
   ttl      = 1
   priority = 31
 }
@@ -111,26 +111,26 @@ resource "cloudflare_record" "mx_web_apps_tech_3" {
 resource "cloudflare_record" "txt_github_pages_challenge_blog" {
   zone_id = data.cloudflare_zone.web_apps_tech.id
 
-  name  = "_github-pages-challenge-nasa9084.blog"
-  type  = "TXT"
-  value = "e5c680fe236c23a6f63234549cb274"
-  ttl   = 1
+  name    = "_github-pages-challenge-nasa9084.blog"
+  type    = "TXT"
+  content = "e5c680fe236c23a6f63234549cb274"
+  ttl     = 1
 }
 
 resource "cloudflare_record" "txt_web_apps_tech" {
   zone_id = data.cloudflare_zone.web_apps_tech.id
 
-  name  = "web-apps.tech"
-  type  = "TXT"
-  value = "\"v=spf1 include:_spf.mx.cloudflare.net ~all\""
-  ttl   = 1
+  name    = "web-apps.tech"
+  type    = "TXT"
+  content = "\"v=spf1 include:_spf.mx.cloudflare.net ~all\""
+  ttl     = 1
 }
 
 resource "cloudflare_record" "txt_dmarc" {
   zone_id = data.cloudflare_zone.web_apps_tech.id
 
-  name  = "_dmarc"
-  type  = "TXT"
-  value = "\"v=DMARC1;  p=none; rua=mailto:4725826b8ac34b6ebdfd417ab2ce571c@dmarc-reports.cloudflare.net\""
-  ttl   = 1
+  name    = "_dmarc"
+  type    = "TXT"
+  content = "\"v=DMARC1;  p=none; rua=mailto:4725826b8ac34b6ebdfd417ab2ce571c@dmarc-reports.cloudflare.net\""
+  ttl     = 1
 }
