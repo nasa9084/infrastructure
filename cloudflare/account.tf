db271c962f68ad520687df9680127aadfe91ce1e
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2026-04-21 16:28:14 +0900

    (squash)

--- cloudflare/account.tf
+++ cloudflare/account.tf
@@ -12,3 +12,7 @@ resource "cloudflare_account" "nasa9084" {
 data "cloudflare_zone" "web_apps_tech" {
   zone_id = cloudflare_zone.web_apps_tech.id
 }
+
+locals {
+  v = data.cloudflare_zone.web_apps_tech.zone_id
+}
