c113f3dbf6c1c1dd210ba090472921782eb67267
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2026-04-21 16:35:09 +0900

    (squash)

--- cloudflare/account.tf
+++ cloudflare/account.tf
@@ -1,18 +1,8 @@
-/*import {
+import {
   id = "abe0a185132e745748442fe5611bfff7"
   to = cloudflare_account.nasa9084
 }
 
 resource "cloudflare_account" "nasa9084" {
   name = "nasa9084"
-}
-*/
-
-
-data "cloudflare_zone" "web_apps_tech" {
-  zone_id = cloudflare_zone.web_apps_tech.id
-}
-
-locals {
-  v = data.cloudflare_zone.web_apps_tech.zone_id
 }
