e2b7655396daadd4bb51d8b7e9183463bd0c3636
nasa9084 <nasa9084@users.noreply.github.com>
2026-07-26 00:52:43 +0900

    Create home-assistant-config GitHub repository (#101)
    * Create home-assistant-config GitHub repository
    
    * Update github/repository_home_assistant_config.tf
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ github/repository_home_assistant_config.tf
@@ -0,0 +1,11 @@
+resource "github_repository" "home-assistant-config" {
+  name = "home-assistant-config"
+
+  allow_merge_commit = false
+  allow_rebase_merge = false
+
+  archive_on_destroy     = true
+  delete_branch_on_merge = true
+
+  has_issues = true
+}
