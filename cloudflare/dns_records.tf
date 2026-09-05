f3f9fff59bbd87bcb0349aee5bf73d531aaf5b2e
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2026-09-06 02:05:18 +0900

    fix drift

--- cloudflare/dns_records.tf
+++ cloudflare/dns_records.tf
@@ -92,41 +92,6 @@ resource "cloudflare_dns_record" "proxmox" {
   proxied = true
 }
 
-
-// //////////////////////////////
-// MX records
-// //////////////////////////////
-
-resource "cloudflare_dns_record" "mx_web_apps_tech_1" {
-  zone_id = cloudflare_zone.web_apps_tech.id
-
-  name     = "web-apps.tech"
-  type     = "MX"
-  content  = "route1.mx.cloudflare.net"
-  ttl      = 1
-  priority = 90
-}
-
-resource "cloudflare_dns_record" "mx_web_apps_tech_2" {
-  zone_id = cloudflare_zone.web_apps_tech.id
-
-  name     = "web-apps.tech"
-  type     = "MX"
-  content  = "route2.mx.cloudflare.net"
-  ttl      = 1
-  priority = 50
-}
-
-resource "cloudflare_dns_record" "mx_web_apps_tech_3" {
-  zone_id = cloudflare_zone.web_apps_tech.id
-
-  name     = "web-apps.tech"
-  type     = "MX"
-  content  = "route3.mx.cloudflare.net"
-  ttl      = 1
-  priority = 31
-}
-
 // //////////////////////////////
 // TXT records
 // //////////////////////////////
@@ -146,20 +111,6 @@ resource "cloudflare_dns_record" "txt_web_apps_tech" {
   name    = "web-apps.tech"
   type    = "TXT"
   content = "\"v=spf1 include:_spf.mx.cloudflare.net ~all\""
-  ttl     = 1
-}
-
-import {
-  id = "3ca8f489f914add59f639139e221d9de/"
-  to = cloudflare_dns_record.txt_cf2024-1
-}
-
-resource "cloudflare_dns_record" "txt_cf2024-1" {
-  zone_id = cloudflare_zone.web_apps_tech.id
-
-  name    = "cf2024-1._domainkey"
-  type    = "TXT"
-  content = "\"v=DKIM1; h=sha256; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiweykoi+o48IOGuP7GR3X0MOExCUDY/BCRHoWBnh3rChl7WhdyCxW3jgq1daEjPPqoi7sJvdg5hEQVsgVRQP4DcnQDVjGMbASQtrY4WmB1VebF+RPJB2ECPsEDTpeiI5ZyUAwJaVX7r6bznU67g7LvFq35yIo4sdlmtZGV+i0H4cpYH9+3JJ78k" "m4KXwaf9xUJCWF6nxeD+qG6Fyruw1Qlbds2r85U9dkNDVAS3gioCvELryh1TxKGiVTkg4wqHTyHfWsp7KD3WQHYJn0RyfJJu6YEmL77zonn7p2SRMvTMP3ZEXibnC9gz3nnhR6wcYL8Q7zXypKTMD58bTixDSJwIDAQAB\""
   ttl     = 1
 }
 
