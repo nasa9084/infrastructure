582227ca83376153eba37f209d94b6a62624152b
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2026-09-06 01:14:47 +0900

    (squash) cloudflare -> local

--- cloudflare/tunnel.tf
+++ cloudflare/tunnel.tf
@@ -7,7 +7,7 @@ resource "cloudflare_zero_trust_tunnel_cloudflared" "homeassistant" {
   account_id = cloudflare_zone.web_apps_tech.account.id
 
   name       = "homeassistant"
-  config_src = "cloudflare"
+  config_src = "local"
 }
 
 import {
@@ -19,5 +19,5 @@ resource "cloudflare_zero_trust_tunnel_cloudflared" "proxmox" {
   account_id = cloudflare_zone.web_apps_tech.account.id
 
   name       = "proxmox"
-  config_src = "cloudflare"
+  config_src = "local"
 }
